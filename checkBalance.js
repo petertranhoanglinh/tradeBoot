34910ad7de0357d8c3664c8363e7e1c81d2041c7
petertranhoanglinh <73209234+petertranhoanglinh@users.noreply.github.com>
2021-10-07 16:18:35 +0700

    update

---
+++ checkBalance.js
@@ -0,0 +1,64 @@
+
+const ccxt = require('ccxt');
+require('dotenv').config();
+const moment = require('moment');
+const binnace = new ccxt.binance({
+    apiKey: process.env.APIKEY,
+    secret: process.env.SECRETKEY,
+});
+
+async function printgetBinance(btcPrice){
+    // goi thong tin tai khoan
+    const balance = await binnace.fetchBalance();
+    const total = balance.total ; 
+    console.log( `USDT của bạn là : ${total.USDT}`);
+    console.log( `BNB của bạn là : ${total.BNB}`);
+}
+async function checkGia(){
+    const priceUSDT = await binnace.fetchOHLCV('MATIC/USDT', '1d', undefined, 30);
+    const priceOJECT = priceUSDT.map(price => {
+        return {
+            timestamp: moment(price[0]).format(),
+            open: price[1], 
+            high:price[2], 
+            low: price[3],
+            close: price[4],
+            volume:price[5]      }
+    })
+
+    const GIATB30NGAYGANNHAT = priceOJECT.reduce((acc, price) => acc + price.close,0)/30;
+
+    
+    // giá hiện tại
+    const lastPrice = priceOJECT[priceOJECT.length-1].close
+   // console.log(priceOJECT);
+    console.log(`GIÁ TRUNG BÌNH 30 NGÀY  Ở BINANCE LÀ ${GIATB30NGAYGANNHAT}`);
+    console.log(`GIÁ  HIỆN TẠI Ở BINANCE LÀ  ${lastPrice}`);
+    
+}
+
+const SAN =  new ccxt.huobi();
+async function checkGiaSan(){
+    var sys = 'ADA/USDT'
+    const priceUSDT = await SAN.fetchOHLCV( sys, '1d', undefined, 30);
+    const priceOBJECT = priceUSDT.map(price => {
+        return {
+            timestamp: moment(price[0]).format(),
+            open: price[1], 
+            high:price[2], 
+            low: price[3],
+            close: price[4],
+            volume:price[5]      }
+    })
+
+    const GIATTB = priceOBJECT.reduce((acc, price) => acc + price.close,0)/30;
+    const lastPrice = priceOBJECT[priceOBJECT.length-1].close
+    //console.log(priceOBJECT);
+    console.log(`GIÁ TRUNG BÌNH 30 NGÀY CỦA ${sys} LÀ : ${GIATTB}`);
+    console.log(`GIÁ HIỆN TẠI  ${lastPrice}`)
+
+}
+
+//printgetBinance();
+//checkGia();
+checkGiaSan();
---
+++ index.js
@@ -0,0 +1,55 @@
+// gọi thư viện 
+const ccxt = require('ccxt');
+const delay =  require('delay');
+const moment = require('moment');
+const binnace = new ccxt.binance({
+    apiKey:'oM9GoHR4BLUijAV3rfKndL2QCLRd06deGtwLrsGERtlnZ9QlvecuBGGC0vWbu6Xa',
+    secret:'190f8XjkqduQVULo8rdmoJOzzOiZQqJiQs403zY3xNLbfEQKn0avGokjzXP23rd0',
+});
+// check tk giả
+binnace.setSandboxMode(true);
+async function printgetBinance(btcPrice){
+    // goi thong tin tai khoan
+    const balance = await binnace.fetchBalance();
+    const total = balance.total;
+    console.log(`Balance: BTC ${total.BTC}, USDT ${total.USDT}`);
+    const totalUSD = (total.BTC - 1) * btcPrice+ total.USDT ;
+    console.log(`total usd: ${totalUSD}.\n`)
+}
+
+async function main(){
+    while (true){
+        await tick();
+        await delay(60  * 1000);
+    }
+}
+
+async function tick(){
+    const price = await binnace.fetchOHLCV('BTC/USDT', '1m', undefined, 20);
+    const bPrice = price.map(price => {
+        return {
+            timestamp: moment(price[0]).format(),
+            open: price[1], 
+            high:price[2], 
+            low: price[3],
+            close: price[4],
+            volume:price[5]      }
+    })
+    // trung bình 20 giá gần nhất.
+    const averagePrice = bPrice.reduce((acc, price) => acc + price.close,0)/20;
+    // giá sau cùng 
+    const lastPrice = bPrice[bPrice.length-1].close
+
+    // thuật toán 
+    const direction = lastPrice > averagePrice ? 'sell' : 'buy'
+    const TRADE_SIDE = 100; 
+    const quantity = 100 / lastPrice 
+    const order  = await binnace.createMarketOrder('BTC/USDT',direction,quantity)
+
+    console.log(`averagePrice : ${averagePrice}. lastPrice : ${lastPrice}`)
+    console.log(`${moment().format()}: ${direction}${quantity} BTC at ${lastPrice}`)
+
+    printgetBinance(lastPrice);
+    
+}
+main();
